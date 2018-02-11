037c80dd48a50efeec083a9f1651232aa7c46d8a
Gustavo Maes <gustavomaescd@gmail.com>
2018-02-11 16:57:46 -0200

    Error manager

--- server.js
+++ server.js
@@ -21,18 +21,43 @@ let route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError);
+
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
     const port = parseInt(val, 10);
 
-    if(isNaN(port)) {
+    if (isNaN(port)) {
         return val;
     }
 
-    if(port >= 0) {
+    if (port >= 0) {
         return port;
     }
 
     return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe' + port :
+        'Port' + port;
+
+        switch (error.code) {
+            case 'EACCES':
+                console.error(bind + ' requires elevanted privileges');
+                process.exit(1);
+                break;
+            case 'EADDRINUSE':
+                console.error(bind + ' is already in use');
+                console.exit(1);
+                break;
+            default:
+                throw error;
+        }
 }
