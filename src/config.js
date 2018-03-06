124ccec43ca3f46d357d617f8c989066c7f338d7
Gustavo Maes <gustavomaescd@gmail.com>
2018-03-06 14:34:00 -0300

    config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = '12e743f0-d335-4b52-9af1-25ffb6798bf2';
+
+module.exports = {
+    connectionString: 'mongodb://wittest.documents.azure.com:10255/wittest?ssl=true',
+    userMongo: 'wittest',
+    passwordMongo: 'NGvHu4vKknTIV3BjnUT04svnaJafzjZD3tN31BySeGr7uJDX7bKLDHcbjGuRLHLnsUe6h7xOYMN16FQw8V7Fwg==',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=witteststorage;AccountKey=WFbYoUjZ1za0hQpmkqfSGvxYq3jf8cHwBa8oj8/1ffJzt+DU+bF5jSxBVqGiO1LsKzpNbiLajdp+398NcejvAg==;EndpointSuffix=core.windows.net'
+}
