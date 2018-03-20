6f2b7dd5fdf679d01b8b2f5b559db52ae6214487
Gustavo Maes <gustavomaescd@gmail.com>
2018-03-20 15:20:50 -0300

    get challenge roules

--- src/routes/challenge-route.js
+++ src/routes/challenge-route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/challenge-controller');
 const authService = require('../services/auth-service');
 
-router.get('/', authService.authorizeAdmin, controller.get);
+router.get('/', authService.authorize, controller.get);
 router.get('/:id', authService.authorize, controller.getById);
 router.get('/class/:id', authService.authorize, controller.getByClass);
 router.post('/', authService.authorizeAdmin, controller.create);
