8f37945f27a2a1016162e6fcc7f557dd55c3c6c8
Gustavo Maes <gustavomaescd@gmail.com>
2018-03-21 15:38:42 -0300

    populate classes

--- src/repositories/student-repository.js
+++ src/repositories/student-repository.js
@@ -9,7 +9,7 @@ exports.get = async () => {
 }
 
 exports.getById = async (id) => {
-    const res = await Student.findById(id, 'classes active _id name email password');
+    const res = await Student.findById(id, 'classes active _id name email password').populate('classes');;
     return res;
 }
 
